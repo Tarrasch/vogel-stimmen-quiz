fa49342726bca51dbd1f87397360402a126a92b7
Arash Rouhani-Kalleh <rouhani@google.com>
2021-11-30 22:09:12 +0100

    Add species selector in the quiz starter

--- src/quiz_starter.ts
+++ src/quiz_starter.ts
@@ -1,7 +1,41 @@
+import { Bird, BIRDS, getSearchParamStringFromBirds } from "./species";
+
 function startQuiz(): void {
     let url = new URL("./quiz.html", document.baseURI);
-    url.searchParams.set("species", "km,bm");
+    url.searchParams.set("species", getSearchParamStringFromBirds(getSelectedSpecies()));
     document.location.href = url.href;
+}
+
+function getSelectedSpecies(): Bird[] {
+    const result: Bird[] = [];
+    for (let bird of BIRDS) {
+        const checkbox = document.getElementById(`specie-${bird.searchParamCode}`) as HTMLInputElement;
+        if (checkbox.checked) {
+            result.push(bird);
+        }
+    }
+    return result;
+}
+
+function getDivWithCheckBox(bird: Bird): HTMLDivElement {
+    const div = document.createElement("div") as HTMLDivElement;
+    const checkbox = document.createElement("input") as HTMLInputElement;
+    const label = document.createElement("label") as HTMLLabelElement;
+
+    checkbox.type = 'checkbox';
+    checkbox.id = `specie-${bird.searchParamCode}`;
+    label.innerText = bird.germanName;
+    div.appendChild(checkbox)
+    div.appendChild(label);
+
+    return div;
+}
+
+function fillSpeciesList(): void {
+    const speciesDiv = document.getElementById('species-list');
+    for (let bird of BIRDS) {
+        speciesDiv?.appendChild(getDivWithCheckBox(bird));
+    }
 }
 
 function addDocumentListeners(): void {
@@ -10,4 +44,5 @@ function addDocumentListeners(): void {
 
 document.addEventListener("DOMContentLoaded", function () {
     addDocumentListeners();
+    fillSpeciesList();
 });
