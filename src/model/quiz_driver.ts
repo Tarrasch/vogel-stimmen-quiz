b4986b4e8e01b35c90e52a034d7254518ee0d8eb
Arash Rouhani-Kalleh <rouhani@google.com>
2021-12-19 17:57:19 +0100

    Use Storage class for storing and seperate it out

--- src/model/quiz_driver.ts
+++ src/model/quiz_driver.ts
@@ -18,30 +18,69 @@ export interface QuizRound {
     recording: Recording,
 }
 
-// stores or fetches and stores Api responses and makes is queryable.
 class Storage {
     readonly birdsInQuiz: Bird[];
+    readonly numRecordingsMap: Map<number, number>;
+    readonly recordingsMap: Map<[number, number], Recording>;
 
     constructor(birdsInQuiz: Bird[]) {
         this.birdsInQuiz = birdsInQuiz;
+        this.numRecordingsMap = new Map();
+        this.recordingsMap = new Map();
+    }
+
+    maybeGetNumRecordings(birdIndex: number): number | undefined {
+        return this.numRecordingsMap.get(birdIndex);
+    }
+
+    maybeGetBirdRecording(birdIndex: number, recordingIndex: number): Recording | undefined {
+        return this.recordingsMap.get([birdIndex, recordingIndex]);
+    }
+
+    saveResult(birdIndex: number, result: XenoCantoApiResponse): void {
+        this.numRecordingsMap.set(birdIndex, result.numRecordings);
+        for (let index = 0; index < result.recordings.length; index++) {
+            const recording: Recording = result.recordings[index];
+            const recordingIndex: number = index + (result.page - 1) * 100;
+            this.recordingsMap.set([birdIndex, recordingIndex], recording);
+        }
+    };
+}
+
+// Stores or fetches and stores Api responses and makes is queryable.
+class Fetcher {
+    readonly birdsInQuiz: Bird[];
+    readonly storage: Storage;
+
+    constructor(birdsInQuiz: Bird[]) {
+        this.birdsInQuiz = birdsInQuiz;
+        this.storage = new Storage(birdsInQuiz);
     }
 
     async getNumRecordingsInXenoCanto(birdIndex: number): Promise<number> {
-        const response: XenoCantoApiResponse = await this.fetchApiResponse({
-            query: buildQuery({ birdQueryName: this.birdsInQuiz[birdIndex].queryName }),
-            pageNumber: 1,
-        });
-        return response.numRecordings;
+        const maybeSavedValue = this.storage.maybeGetNumRecordings(birdIndex);
+        if (!maybeSavedValue) {
+            const response = await this.fetchApiResponse({
+                query: buildQuery({ birdQueryName: this.birdsInQuiz[birdIndex].queryName }),
+                pageNumber: 1,
+            });
+            this.storage.saveResult(birdIndex, response);
+            return response.numRecordings;
+        }
+        return maybeSavedValue;
     }
 
     async getBirdRecording(birdIndex: number, recordingIndex: number): Promise<Recording> {
-        const response: XenoCantoApiResponse = await this.fetchApiResponse({
-            query: buildQuery({ birdQueryName: this.birdsInQuiz[birdIndex].queryName }),
-            pageNumber: recordingIndex / 100 + 1,
-        });
-        console.log(response);
-
-        return response.recordings[recordingIndex % 100]
+        const maybeSavedValue = this.storage.maybeGetBirdRecording(birdIndex, recordingIndex);
+        if (!maybeSavedValue) {
+            const response = await this.fetchApiResponse({
+                query: buildQuery({ birdQueryName: this.birdsInQuiz[birdIndex].queryName }),
+                pageNumber: recordingIndex / 100 + 1,
+            });
+            this.storage.saveResult(birdIndex, response);
+            return response.recordings[recordingIndex % 100]
+        };
+        return maybeSavedValue;
     }
 
     private fetchApiResponse(parameters: XenoCantoParameters): Promise<XenoCantoApiResponse> {
@@ -53,17 +92,17 @@ class Storage {
 
 export class QuizDriver {
     readonly birdsInQuiz: Bird[];
-    readonly storage: Storage;
+    readonly fetcher: Fetcher;
 
     constructor(birdsInQuiz: Bird[]) {
         this.birdsInQuiz = birdsInQuiz;
-        this.storage = new Storage(birdsInQuiz);
+        this.fetcher = new Fetcher(birdsInQuiz);
     }
 
     async getNewRound(): Promise<QuizRound> {
         const birdIndex = getRandomInt(this.birdsInQuiz.length);
-        const recordingIndex = getRandomInt(await this.storage.getNumRecordingsInXenoCanto(birdIndex));
-        const recording = await this.storage.getBirdRecording(birdIndex, recordingIndex);
+        const recordingIndex = getRandomInt(await this.fetcher.getNumRecordingsInXenoCanto(birdIndex));
+        const recording = await this.fetcher.getBirdRecording(birdIndex, recordingIndex);
         return {
             correctBird: this.birdsInQuiz[birdIndex],
             recording: recording,
