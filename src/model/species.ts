e2be3eed58c2ced393a5d6a15e715ba6d78b6a9e
Arash Rouhani <arash.rouhani@gmail.com>
2023-04-23 23:11:01 +0200

    Rethink species.ts - remove birds without song
    Yet there's an intention to re-add them and mark them as call-only
    birds.

--- src/model/species.ts
+++ src/model/species.ts
@@ -1,20 +1,40 @@
 export interface Bird {
     scientificName: string;
     germanName: string;
+    difficulty: BirdDifficulty;
+}
+
+export enum BirdDifficulty {
+    EASY,
+    MEDIUM,
+    HARD,
 }
 
 export const BIRDS: Bird[] = [
-    { scientificName: "Parus Major", germanName: "Kohlmeise", },
-    { scientificName: "Cyanistes Caeruleus", germanName: "Blaumeise", },
-    { scientificName: "Periparus Ater", germanName: "Tannenmeise", },
-    { scientificName: "Fringilla Coelebs", germanName: "Buchfink", },
-    { scientificName: "Dendrocopos Major", germanName: "Buntspecht", },
-    { scientificName: "Troglodytes Troglodytes", germanName: "Zaunkönig", },
-    { scientificName: "Erithacus Rubecula", germanName: "Rotkehlchen", },
-    { scientificName: "Phylloscopus Collybita", germanName: "Zilpzalp", },
-    { scientificName: "Sitta Europaea", germanName: "Kleiber", },
-    { scientificName: "Garrulus Glandarius", germanName: "Eichelhäher", },
-]
+    { scientificName: "Anas Platyrhynchos", germanName: "Stockente", difficulty: BirdDifficulty.EASY },
+    { scientificName: "Cinclus Cinclus", germanName: "Wasseramsel", difficulty: BirdDifficulty.HARD },
+    { scientificName: "Cyanistes Caeruleus", germanName: "Blaumeise", difficulty: BirdDifficulty.EASY },
+    { scientificName: "Erithacus Rubecula", germanName: "Rotkehlchen", difficulty: BirdDifficulty.MEDIUM },
+    { scientificName: "Fringilla Coelebs", germanName: "Buchfink", difficulty: BirdDifficulty.EASY },
+    { scientificName: "Hirundo Rustica", germanName: "Rauchschwalbe", difficulty: BirdDifficulty.MEDIUM },
+    { scientificName: "Parus Major", germanName: "Kohlmeise", difficulty: BirdDifficulty.EASY },
+    { scientificName: "Periparus Ater", germanName: "Tannenmeise", difficulty: BirdDifficulty.MEDIUM },
+    { scientificName: "Phylloscopus Collybita", germanName: "Zilpzalp", difficulty: BirdDifficulty.EASY },
+    { scientificName: "Regulus Ignicapilla", germanName: "Sommergoldhähnchen", difficulty: BirdDifficulty.HARD },
+    { scientificName: "Regulus Regulus", germanName: "Wintergoldhähnchen", difficulty: BirdDifficulty.HARD },
+    { scientificName: "Troglodytes Troglodytes", germanName: "Zaunkönig", difficulty: BirdDifficulty.MEDIUM },
+];
+
+// I've not included the following birds because they seem to have only calls and not songs.
+// This could also explain why they have so few results for songs in xeno-canto.
+//
+// * Sperber
+// * Eisvogel
+// * Graureiher
+// * Buntspecht
+// * Eichelhäher
+// * Kormoran
+// * Kleiber
 
 export function getUniqueName(bird: Bird): string {
     return bird.scientificName.replace(" ", "-");
